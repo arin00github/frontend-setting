0397dac204f07d3a23e7ac67148056b7ffa245bb
DESKTOP-3M1TBJ3\USER <leeleejin325@gmail.com>
2021-03-15 10:58:25 +0900

    project initialState

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+
+    <div>
+      <head>
+        <title>next setting</title>
+      </head>
+      <h1>Next Js Study</h1>
+    </div>
+  )
+}
